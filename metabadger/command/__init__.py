8a00a9e477fb60933fb769ae7b28ae7a7df0e101
patel-ashish <patel.ashish@salesforce.com>
2021-12-17 14:51:45 -0800

    cloudwatch metrics init

--- metabadger/command/__init__.py
+++ metabadger/command/__init__.py
@@ -2,3 +2,4 @@ from metabadger.command import disable_metadata
 from metabadger.command import discover_metadata
 from metabadger.command import discover_role_usage
 from metabadger.command import harden_metadata
+from metabadger.command import cloudwatch_metrics
